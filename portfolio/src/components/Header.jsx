8cda9c3aaaea87876b22c0ab3044cd8bc176801d
kiwiicam <mastercamnz@gmail.com>
2024-11-22 14:39:33 +1300

    dark mode in the works

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -1,21 +1,51 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { MdOutlineDarkMode } from "react-icons/md";
+import { MdOutlineLightMode } from "react-icons/md";
 import { RiMenu5Line } from "react-icons/ri";
 import { RxCross1 } from "react-icons/rx";
 import '../css/Header.css';
 import '../css/Global.css';
 function Header() {
     const [show, setShow] = useState(false);
-
+    const [theme,setTheme] = useState(false);// false indicates light mode, true indicates dark mode
     function handleClick() {
         setShow(!show);
     }
+
+    function changeThemeState(){
+        setTheme(!theme);
+    }
+
+    function changeTheme() {
+        localStorage.setItem("theme", "dark")
+        changeThemeState();
+    }
+
+    function changeThemeLight(){
+        localStorage.setItem("theme", "light");
+        changeThemeState();
+    }
+
+    useEffect(() => {
+        const theme = localStorage.getItem('theme');
+        if (theme.localeCompare('dark') === 0) {
+            document.documentElement.style.setProperty('--black', '#ffffff');
+            document.documentElement.style.setProperty('--white', '#000000');
+        }
+        else if(theme.localeCompare('light') === 0){
+            document.documentElement.style.setProperty('--black', '#000000');
+            document.documentElement.style.setProperty('--white', '#ffffff');
+        }
+    }, [theme]);
 
     const screenWidth = window.screen.width;
     return (
         <div className='navbar'>
             <div className='icon'>
-                <MdOutlineDarkMode id='dark' />
+                {theme ?
+                    <MdOutlineDarkMode id='dark' onClick={changeThemeLight} /> :
+                    <MdOutlineLightMode id='dark' onClick={changeTheme}/>
+                }
             </div>
             {screenWidth < 800 ?
                 (show ? <><RxCross1 id='cross' onClick={handleClick} />
